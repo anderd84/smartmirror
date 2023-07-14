1778a878bd91c6c075cf53ab77682a85a00930f3
WinstonPrice <113219264+WinstonPrice@users.noreply.github.com>
2023-07-13 22:04:35 -0700

    Added in functions and docstrings

--- guielements.py
+++ guielements.py
@@ -1,5 +1,8 @@
 '''Program Design: 
-    1. '''
+    1. import all necessary libraries
+    2. initialize the window
+    3. create the GUI class 
+    4. create a function for each intended integration'''
 
 #imports
 from tkinter import * 
@@ -14,3 +17,64 @@ import pickle
 #from newsapi.articles import Articles
 
 #google imports 
+
+WIDTH = 1920 
+HEIGHT = 1080
+
+# Weather API
+
+#News API credentials
+
+class GUI(Frame):
+
+    def __init__():
+        pass
+
+    def setup_GUI():
+        '''This function will set up the GUI and all of its elements'''
+        pass
+
+    def weather():
+        '''This function will display the weather on the GUI'''
+        pass
+
+    def news():
+        '''This function will display the news on the GUI using a news API'''
+        pass
+
+    def calendar():
+        '''This function will display the calendar based on the user's google calendar'''
+        pass
+
+    def clock():
+        '''This function will display the clock'''
+        pass
+
+    def quote():
+        '''This function will display a quote quote of the day'''
+        pass
+
+    def todo():
+        '''This function will display the user's to do list, (Might be taken out, IDK yet)'''
+        pass
+
+    def reminders():
+        '''This function will display the user's reminders, (Also might be taken out, IDK yet)'''
+        pass
+
+    def gmail():
+        '''This function will display the user's gmail'''
+        pass
+
+    def spotify():
+        '''This function will display the user's spotify, inculding the current song playing, playlist, etc.'''
+        pass
+
+    def youtube():
+        '''This function will display the user's youtube, including the current video playing'''
+        pass
+
+    def google():
+        '''This function will display a google search bar'''
+        pass
+
