00648e3ade8ffe33d60b9f56c39d811fbae0af25
WinstonPrice <113219264+WinstonPrice@users.noreply.github.com>
2023-07-13 18:35:44 -0700

    Added in imports

--- guielements.py
+++ guielements.py
@@ -1,2 +1,16 @@
 '''Program Design: 
     1. '''
+
+#imports
+from tkinter import * 
+import tkinter.font
+import time
+from time import strftime
+from darksky import forecast
+from datetime import date, timedelta
+import pandas as pd
+import os.path 
+import pickle
+#from newsapi.articles import Articles
+
+#google imports 
